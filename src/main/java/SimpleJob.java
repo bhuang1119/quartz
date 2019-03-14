a79360823d2a317167f76b8ce2aa3fac353c774f
huangbin <huangbin@souche.com>
2019-03-14 20:12:00 +0800

    quartz

---
+++ src/main/java/SimpleJob.java
@@ -0,0 +1,11 @@
+/**
+ * @Author: huangbin
+ * @Description:
+ * @Date: Created in 
+ * @Modified By:
+ */
+public class SimpleJob {
+    public void run(){
+        System.out.println("simpleJob任务执行。。。。");
+    }
+}
